aab114f69caab4bc54f872bf413f358a282850bc
bluberrywaffl <127812144+bluberrywaffl@users.noreply.github.com>
2023-05-02 12:29:15 +0900

    adjust date location 2

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -26,7 +26,7 @@ const TodoItem = ({ todo, onToggle, onDelete }) => {
       >
         {todo.text}
       </span>
-      <span className={`${styles.date} ${styles.dateMarginLeft}`}>{formattedDate}</span>
+      <span className={`${styles.date} ${styles.dateMarginRight}`}>{formattedDate}</span>
       {/* 삭제 버튼을 렌더링하고, 클릭 시 onDelete 함수를 호출하여 해당 할 일을 삭제합니다. */}
       <button onClick={onDelete}>Delete</button>
     </li>
